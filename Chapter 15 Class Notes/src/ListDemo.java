96c3b0065715965b5bac617761926a78789674c2
Michael-Mihailov <111469137+Michael-Mihailov@users.noreply.github.com>
2023-09-15 14:10:26 -0500

    changed stuff
    hi

--- Chapter 15 Class Notes/src/ListDemo.java
+++ Chapter 15 Class Notes/src/ListDemo.java
@@ -9,6 +9,37 @@ public class ListDemo
 {
     public static void main(String[] args)
     {
+        LinkedList<String> staff = new LinkedList();
+
+        staff.addLast("Clint");
+        staff.addLast("Thor");
+        staff.addLast("Michael");
+        staff.addLast("Loki is cool");
+
+        ListIterator<String> iter = staff.listIterator();
+        iter.next();
+
+        String name = iter.next();
+        System.out.println(name);
+
+        iter.add("joe");
+        iter.add("bruce");
+
+        iter.next(); iter.remove();
+
+        iter.previous(); iter.set("Scott");
+
+        for (String n : staff)
+        {
+            System.out.println(n);
+        }
+
         
+
+        iter = staff.listIterator(); // reset the iterator
+
+
+        
+
     }
 }
