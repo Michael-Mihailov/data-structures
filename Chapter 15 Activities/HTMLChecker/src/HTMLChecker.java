299348cc2d1a867154a70ddfd1ff23c80b2f4beb
Michael-Mihailov <111469137+Michael-Mihailov@users.noreply.github.com>
2023-10-09 13:38:33 -0500

    10/9/2023 changes

--- Chapter 15 Activities/HTMLChecker/src/HTMLChecker.java
+++ Chapter 15 Activities/HTMLChecker/src/HTMLChecker.java
@@ -17,13 +17,38 @@ public class HTMLChecker
 {
     public static void main(String[] args)
     {
-        String filename = "src/TagSample1.html";
+        String filename = "Chapter 15 Activities/HTMLChecker/src/TagSample3.html";
+
+        Stack<String> hStack = new Stack<>();
 
         try (Scanner in = new Scanner(new File(filename)))
         {
             // Your code goes here
-            . . .
-
+            while (in.hasNext())
+            {
+                String s = in.next(); //System.out.println(s);
+                if (s.indexOf('/') == -1)
+                {
+                    hStack.push(s);
+                }
+                else
+                {
+                    if (hStack.size() == 0)
+                    {
+                        System.out.println("Error1!");
+                        return;
+                    }
+                    if (!hStack.pop().equals(s.replace("/", "")))
+                    {
+                        System.out.println("Error2!");
+                        return;
+                    }
+                }
+            }
+            if (hStack.size() != 0)
+            {
+                System.out.println("Error3!");
+            }
 
         } catch (FileNotFoundException e)
         {
